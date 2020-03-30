c2c7d77f14194674a42b45dd0c95dd432fec6351
Piotr Styczyński <piotrs@radcode.co>
2020-03-30 13:03:38 +0200

    Update README and Python package info

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
 setup(
     name='eniam',
-    version='0.1',
+    version='0.9',
     license='MIT',
     author='styczynski',
     author_email='piotr@styczynski.in',
     url='http://styczynski.in',
-    long_description="README.txt",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=[],
-    scripts=['eniam.py'],
+    scripts=['bin/eniam-cli'],
     include_package_data=True,
     package_data={},
-    description="Cross-platform python wrapper around ENIAM ()",
+    description="Cross-platform python wrapper around ENIAM (http://eniam.nlp.ipipan.waw.pl/)",
 )
